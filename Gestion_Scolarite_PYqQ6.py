7ab5ecf43efdae012edc079bd9cf6cdd0634dd6f
ismail703 <ismailxelm@gmail.com>
2025-01-09 15:15:20 +0100

    Upload f Gestion_Scolarite_PyQt6.py last version

--- Gestion_Scolarite_PYqQ6.py
+++ Gestion_Scolarite_PYqQ6.py
@@ -31,16 +31,23 @@ class Enseignant(QMainWindow):
         form_layout.addRow("Département:", self.departement_input)
         self.layout.addLayout(form_layout)
 
+        self.clear_btn = QPushButton("Effacer les Champs")
+        self.clear_btn.setFixedWidth(150)
+        self.clear_btn.clicked.connect(self.clear_inputs)
+
         button_layout = QHBoxLayout()
+        button_layout.addSpacing(20)
+
         self.add_btn = QPushButton("Ajouter Enseignant")
-        self.add_btn.setFixedWidth(200)
+        self.add_btn.setFixedWidth(150)
         self.add_btn.clicked.connect(self.ajouter_enseignant)
-        button_layout.addWidget(self.add_btn, alignment=Qt.AlignmentFlag.AlignCenter)
+        button_layout.addWidget(self.clear_btn)
+        button_layout.addWidget(self.add_btn)
         self.layout.addLayout(button_layout)
 
         self.table = QTableWidget()
-        self.table.setColumnCount(5)
-        self.table.setHorizontalHeaderLabels(["Nom", "Prénom", "CIN", "Département", "Supprimer"])
+        self.table.setColumnCount(6)
+        self.table.setHorizontalHeaderLabels(["Nom", "Prénom", "CIN", "Département", "Supprimer", "Modifier"])
         self.layout.addWidget(self.table)
 
         self.lister_enseignant()
@@ -89,19 +96,22 @@ class Enseignant(QMainWindow):
                 del_btn = QPushButton("Supprimer")
                 del_btn.clicked.connect(lambda _, id=enseignants[i][0]: self.supprimer_enseignant(id))
                 self.table.setCellWidget(i, 4, del_btn)
+
+                update_btn = QPushButton("Modifier")
+                update_btn.clicked.connect(lambda _, id=enseignants[i][0]: self.modifier_enseignant(id))
+                self.table.setCellWidget(i, 5, update_btn)
         except sqlite3.Error as e:
             QMessageBox.critical(self, "Erreur", f"Une erreur est survenue: {e}")
 
     def supprimer_enseignant(self, ens_id):
         try:
-            print(ens_id)
             conn = connect_db()
             curs = conn.cursor()
             curs.execute("DELETE FROM Enseignant WHERE id = ?", (ens_id,))
             conn.commit()
             conn.close()
 
-            QMessageBox.information(self, "Succès", "Enseignant supprimé avec succès!")
+            QMessageBox.information(self, "Succès", f"Enseignant avec l'id {ens_id} supprimé!")
             self.lister_enseignant()
         except sqlite3.Error as e:
             QMessageBox.critical(self, "Erreur", f"Une erreur est survenue: {e}")
@@ -111,17 +121,61 @@ class Enseignant(QMainWindow):
         self.cin_input.clear()
         self.departement_input.clear()
 
-    def existe(self, id):
-        conn = connect_db()
-        curs = conn.cursor()
+        self.switch_ajout()
 
-        curs.execute("SELECT * FROM Enseignant WHERE id = ?", (id,))
-        res = curs.fetchone()
+    def modifier_enseignant(self, ens_id):
+        try:
+            conn = connect_db()
+            curs = conn.cursor()
+            curs.execute("SELECT nom, prenom, cin, departement FROM Enseignant WHERE id = ?", (ens_id,))
+            enseignant = curs.fetchone()
+            conn.close()
 
-        conn.close()
+            if enseignant:
+                self.nom_input.setText(enseignant[0])
+                self.prenom_input.setText(enseignant[1])
+                self.cin_input.setText(enseignant[2])
+                self.departement_input.setText(enseignant[3])
 
-        return res is not None
+                self.add_btn.setText("Modifier Enseignant")
+                self.add_btn.clicked.disconnect()
+                self.add_btn.clicked.connect(lambda: self.applique_Modification(ens_id))
+            else:
+                QMessageBox.critical(self, "Erreur", f"Aucun enseignant trouvé avec l'id {ens_id}!")
+        except sqlite3.Error as e:
+            QMessageBox.critical(self, "Erreur", f"Une erreur est survenue: {e}")
 
+    def applique_Modification(self, ens_id):
+        nom = self.nom_input.text()
+        prenom = self.prenom_input.text()
+        cin = self.cin_input.text()
+        departement = self.departement_input.text()
+
+        if not nom or not prenom or not cin or not departement:
+            QMessageBox.warning(self, "Erreur", "Tous les champs doivent être remplis!")
+            return
+
+        try:
+            conn = connect_db()
+            curs = conn.cursor()
+            curs.execute(""" UPDATE Enseignant 
+               SET nom = ?, prenom = ?, cin = ?, departement = ? 
+               WHERE id = ?;""", (nom, prenom, cin, departement, ens_id))
+            conn.commit()
+            conn.close()
+
+            QMessageBox.information(self, "Succès", "Enseignant modifié avec succès!")
+            self.clear_inputs()
+            self.lister_enseignant()
+
+            self.switch_ajout()
+        except sqlite3.Error as e:
+            QMessageBox.critical(self, "Erreur", f"Une erreur est survenue: {e}")
+
+    def switch_ajout(self):
+        self.add_btn.setText("Ajouter Enseignant")
+        self.add_btn.clicked.disconnect()
+        self.add_btn.clicked.connect(self.ajouter_enseignant)
 
 # Run the application
 app = QApplication([])
